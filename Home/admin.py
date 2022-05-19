e61686496bfd11f095d65d97b41806880b0eb3c7
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2022-05-19 18:33:13 +0530

    Changed the model name

--- Home/admin.py
+++ Home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from Home.models import Contact
+from Home.models import Home_contact
 
 # Register your models here.
 #NOTE FROM ME: After creating the models, they have to be registered within the admin.py
-admin.site.register(Contact)
+admin.site.register(Home_contact)
